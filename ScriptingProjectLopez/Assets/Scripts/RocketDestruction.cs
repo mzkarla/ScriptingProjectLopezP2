4af4b3e73396ed63d937f6ce918424453c045f4b
mzkarla <714875@student.dusd.net>
2023-03-30 09:53:50 -0700

    push

--- ScriptingProjectLopez/Assets/Scripts/RocketDestruction.cs
+++ ScriptingProjectLopez/Assets/Scripts/RocketDestruction.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RocketDestruction : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        Destroy(gameObject, 1.5f)
+;    }
 
     // Update is called once per frame
     void Update()
